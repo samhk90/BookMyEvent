d71c33fc948df1e7e217668deab736ba18f25702
Mohammed Samee <86250719+samhk90@users.noreply.github.com>
2023-12-29 22:56:36 +0530

    Update settings.py

--- bookmyevent/settings.py
+++ bookmyevent/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tlin#w16@a7e40(h+95imh1#(h_(@f3qo#xee%863473ul!!-*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
